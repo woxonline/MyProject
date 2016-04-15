fb95d03054ccef5ec9ca352349b8e484d0dd59dc
woxonline <save_lord@hotmail.com>
2016-04-15 13:13:57 +0700

    Added files via upload

--- src/templatemethod/TemplateMethod.java
+++ src/templatemethod/TemplateMethod.java
@@ -1,10 +1,11 @@
 package templatemethod;
+// Author By Thiti Kitiwakul
 
 public class TemplateMethod {
     public static void main(String[] args) {
         AccountCreator pc = new PantipCreator();
         AccountCreator fc = new FacebookCreator();
         pc.CreatorAccount();
-        //fc.CreatorAccount();
+        fc.CreatorAccount();
     }
 }
